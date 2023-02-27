ead2515bcf25ffa61f2266459d73c16ec31153df
Thibault PARES <tpares@groupeastek.fr>
2023-02-27 16:57:45 +0100

    add debug traces

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -113,9 +113,11 @@ const footerTexteButton = ref('Inscrire mon organisme');
 const getData = async () => {
   try {
     const dataOrganismes = await axios.get('http://localhost:1337/api/organismes?populate=*');
+    console.log(dataOrganismes);
     organismes.value = dataOrganismes.data.data.map((organisme) => ({
       ...organisme,
       title: organisme.attributes.nom,
+      // TODO: make it work
       img: organisme.attributes.img.data[0].attributes.url,
       description: organisme.attributes.description,
       website: organisme.attributes.website,
@@ -135,8 +137,9 @@ const getData = async () => {
         name: perimeter.attributes.perimetres,
       })).reduce((a, b) => ({ ...a, [b.id]: b.name }), {})),
     }));
-    console.log(organismes.value);
+    console.log(organismes);
   } catch (error) {
+    console.log(error);
     $q.notify({
       message: 'Erreur lors du chargement des organismes',
       caption: 'Merci de réesayer ultérieurement',
