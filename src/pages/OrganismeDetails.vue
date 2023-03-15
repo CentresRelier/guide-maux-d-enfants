ce87ee6d12c5ef48e2682ad5de2737d7b0d859c0
Thibault <tpares@groupeastek.fr>
2023-03-15 10:00:48 +0100

    Fix default image not displaying in OrganismeDetails

--- src/pages/OrganismeDetails.vue
+++ src/pages/OrganismeDetails.vue
@@ -278,7 +278,7 @@ const organisme = ref({
 });
 
 function getOrganismeImage(dataOrganisme) {
-  if (dataOrganisme.data.data.attributes.img.data.attributes.url !== null) {
+  if (dataOrganisme.data.data.attributes.img.data !== null) {
     organisme.value.img = `${SERVER_PATH}${dataOrganisme.data.data.attributes.img.data.attributes.url}`;
   } else {
     organisme.value.img = '/statics/default-organisme-image.jpg';
