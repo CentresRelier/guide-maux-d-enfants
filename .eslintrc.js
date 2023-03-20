2d748ba28db15570c46a4ece81a7594b2ca4cad7
Thibault Pares <pares.thibault@outlook.com>
2023-03-20 09:21:21 +0100

    Fix error 'linebreaks expected to be LF' again

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,14 +27,14 @@ module.exports = {
     // 'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
 
     'airbnb-base'
-    
+
   ],
 
   plugins: [
     // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
     // required to lint *.vue files
     'vue',
-    
+
   ],
 
   globals: {
@@ -52,7 +52,7 @@ module.exports = {
 
   // add your custom rules here
   rules: {
-    
+
     'no-param-reassign': 'off',
     'no-void': 'off',
     'no-nested-ternary': 'off',
@@ -67,10 +67,10 @@ module.exports = {
     'import/no-unresolved': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
-    
+
     'prefer-promise-reject-errors': 'off',
 
-    "linebreak-style": ["error", (process.platform === "win32" ? "windows" : "unix")], // https://stackoverflow.com/q/39114446/2771889
+    "linebreak-style": [2, (process.platform === "win32" ? "windows" : "unix")], // https://stackoverflow.com/q/39114446/2771889
 
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
