0bae4677a60ea189c5e6444ae834b22b314757d9
MorelVictor <morel.victor1@gmail.com>
2023-10-13 18:12:46 +0200

    feat(home): refactor categories and ageRange

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,43 +1,14 @@
 <template>
   <q-card class="card-categories q-pr-sm q-pl-sm q-hoverable">
     <div class="row-categories row q-pt-lg">
-      <FilterButton
-        :buttonText="'Addiction'"
-        :urlIcon="addictionUrl"
-        :tooltip="'Drogue, écrans, tabac, alcool, pornographie, sexe...'"
-        v-on:filterSelected="isNotInLocalStorage('Addiction')"
-        v-on:isInLocaleStorage="isInLocalStorage('Addiction')"/>
-      <FilterButton
-        :buttonText="'Violence'"
-        :urlIcon="violenceUrl"
-        :tooltip="'Violences physiques, sexuelles, psychologiques, cyber-violences...'"
-        v-on:filterSelected="isNotInLocalStorage('Violence')"
-        v-on:isInLocaleStorage="isInLocalStorage('Violence')"/>
-      <FilterButton
-        :buttonText="'Sexualité'"
-        :urlIcon="sexualityUrl"
-        :tooltip="'Prévention, genre, orientation sexuelle, prostitution...'"
-        v-on:filterSelected="isNotInLocalStorage('Sexualité')"
-        v-on:isInLocaleStorage="isInLocalStorage('Sexualité')"/>
-      <FilterButton
-        :buttonText="'Harcèlement'"
-        :urlIcon="harasmentUrl"
-        :tooltip="'Scolaire, périscolaire, cyberharcèlement, intrafamilial, harcèlement de rue...'"
-        v-on:filterSelected="isNotInLocalStorage('Harcèlement')"
-        v-on:isInLocaleStorage="isInLocalStorage('Harcèlement')"/>
-      <FilterButton
-        :buttonText="'Santé mentale'"
-        :urlIcon="mentalHealthUrl"
-        :tooltip="'Dépression, phobies, envies suicidaires, anxiété, isolement...'"
-        v-on:filterSelected="isNotInLocalStorage('Santé mentale')"
-        v-on:isInLocaleStorage="isInLocalStorage('Santé mentale')"/>
-      <FilterButton
-        :buttonText="'Discrimination'"
-        :urlIcon="discriminationUrl"
-        :tooltip="`Raciale, sociale, religieuse, sexiste, culturelle, transphobie,
-                basée sur l'orientation sexuelle, l'apparence physique, le handicap...`"
-        v-on:filterSelected="isNotInLocalStorage('Discrimination')"
-        v-on:isInLocaleStorage="isInLocalStorage('Discrimination')"/>
+      <div v-for="button in Buttons" :key="button.id">
+        <FilterButton
+          :buttonText="button.text"
+          :urlIcon="button.url"
+          :tooltip="button.tooltip"
+          v-on:filterSelected="isNotInLocalStorage(button.text)"
+          v-on:isInLocaleStorage="isInLocalStorage(button.text)"/>
+      </div>
     </div>
     <div class="button-container row">
     </div>
@@ -53,12 +24,39 @@ export default {
 import FilterButton from 'components/FilterButton.vue';
 import { ref } from 'vue';
 
-const addictionUrl = ref('statics/thematique-icons/addiction.png');
-const violenceUrl = ref('statics/thematique-icons/violence.png');
-const discriminationUrl = ref('statics/thematique-icons/discrimination.png');
-const harasmentUrl = ref('statics/thematique-icons/harcelement.png');
-const mentalHealthUrl = ref('statics/thematique-icons/santementale.png');
-const sexualityUrl = ref('statics/thematique-icons/sexualite.png');
+const Buttons = ref([
+  {
+    text: 'Addiction',
+    url: 'statics/thematique-icons/addiction.png',
+    tooltip: 'Drogue, écrans, tabac, alcool, pornographie, sexe...',
+  },
+  {
+    text: 'Violence',
+    url: 'statics/thematique-icons/violence.png',
+    tooltip: 'Violences physiques, sexuelles, psychologiques, cyber-violences...',
+  },
+  {
+    text: 'Sexualité',
+    url: 'statics/thematique-icons/discrimination.png',
+    tooltip: 'Prévention, genre, orientation sexuelle, prostitution...',
+  },
+  {
+    text: 'Harcèlement',
+    url: 'statics/thematique-icons/harcelement.png',
+    tooltip: 'Scolaire, périscolaire, cyberharcèlement, intrafamilial, harcèlement de rue...',
+  },
+  {
+    text: 'Santé mentale',
+    url: 'statics/thematique-icons/santementale.png',
+    tooltip: 'Dépression, phobies, envies suicidaires, anxiété, isolement...',
+  },
+  {
+    text: 'Discrimination',
+    url: 'statics/thematique-icons/sexualite.png',
+    tooltip: `Raciale, sociale, religieuse, sexiste, culturelle, transphobie,
+             basée sur l'orientation sexuelle, l'apparence physique, le handicap...`,
+  },
+]);
 
 const selectedFilters = ref([]);
 const emit = defineEmits(['filtersUpdated']);
