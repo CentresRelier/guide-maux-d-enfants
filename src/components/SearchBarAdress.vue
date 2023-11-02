0cacfd265dabdae04ecc75231b1f45ba022be935
MorelVictor <morel.victor1@gmail.com>
2023-11-02 16:07:34 +0100

    feat(searchBar): add new searchBar

--- src/components/SearchBarAdress.vue
+++ src/components/SearchBarAdress.vue
@@ -1,21 +1,20 @@
 <template>
   <q-input dense debounce="400" v-model="query" class="search" type="text"
-            placeholder="Entrez une ville ou un code postal"
-            borderless
-            @keyup.enter="searchAdress"
-            @keyup.delete="searchAdress"
-            @keyup.esc="searchAdress">
+           placeholder="Entrez une adresse ou ville min 3 caractères"
+           borderless
+  >
     <template v-slot:prepend>
       <q-icon name="room" class="search-button icon-search q-pl-sm"/>
     </template>
     <template v-slot:append>
       <q-icon name="search" class="search-button q-pr-sm cursor-pointer"
-                @click="searchAdress"/>
+                @click="searchAddress"/>
     </template>
   </q-input>
   <q-list
       v-if="searchResults.length > 0"
       bordered
+      class="list"
     >
       <q-item
         v-for="address in searchResults"
@@ -28,60 +27,80 @@
     </q-list>
 </template>
 
-<script>
-// import axios from 'axios';
+<script setup>
+import {
+  ref,
+  defineEmits,
+  watch,
+} from 'vue';
+import axios from 'axios';
 
-export default {
-  name: 'searchbar-component',
-  emits: ['inputSubmitted'],
-  data() {
-    return {
-      query: '',
-      searchResults: [],
-    };
-  },
-  methods: {
-    async searchAdress() {
-      if (this.query.length >= 3) {
-        const API_URL = `https://api-adresse.data.gouv.fr/search/?q=${this.query}`;
+const searchResults = ref([]);
+const query = ref([]);
 
-        try {
-          const response = await fetch(API_URL);
-          if (response.ok) {
-            const data = await response.json();
-            const adresses = data.features;
-            this.searchResults = adresses.map((adress) => {
-              const props = adress.properties;
-              const ville = props.city;
-              const departement = props.context.split(',')[0];
-              const region = props.context.split(',')[1];
-              return {
-                ville,
-                departement,
-                region,
-                label: props.label,
-              };
-            });
-            this.$emit('inputSubmitted', this.searchResults);
-            console.log(this.searchResults);
-          } else {
-            this.searchResults = [];
-            this.$emit('inputSubmitted', this.searchResults);
-            console.log('Erreur lors de la requête API');
-          }
-        } catch (error) {
-          console.error('Erreur réseau', error);
-        }
+const finalSearchObject = ref({
+  ville: '',
+  department: '',
+  departmentPostalCode: '',
+  region: '',
+});
+
+const emit = defineEmits(['inputSubmitted', 'finalSearchObject']);
+
+const searchAddress = async () => {
+  if (query.value.length >= 3) {
+    const API_URL = `https://api-adresse.data.gouv.fr/search/?q=${query.value}`;
+
+    try {
+      const response = await axios.get(API_URL);
+      if (response.status === 200) {
+        const { data } = response;
+        const addresses = data.features;
+        searchResults.value = addresses.map((address) => {
+          const props = address.properties;
+          const ville = props.city;
+          const departement = props.context.split(', ')[1];
+          const departementPostalCode = props.context.split(', ')[0];
+          const region = props.context.split(', ').pop();
+          return {
+            ville,
+            departement,
+            departementPostalCode,
+            region,
+            label: props.label,
+          };
+        });
+      } else {
+        searchResults.value = [];
+
+        console.log('Erreur lors de la requête API');
       }
-    },
-    selectAddress(address) {
-      this.query = address.label;
-      this.searchResults = [];
-      console.log('Ville:', address.ville);
-      console.log('Région:', address.region);
-      console.log('Département:', address.departement);
-    },
-  },
+    } catch (error) {
+      console.error('Erreur réseau', error);
+    }
+  }
+};
+
+const preventReload = ref(true);
+
+watch(query, (newQuery) => {
+  if (newQuery.length === 0) {
+    searchResults.value = [];
+  } else if (preventReload.value && newQuery.length >= 3) {
+    searchAddress();
+  }
+  preventReload.value = true;
+});
+
+const selectAddress = (address) => {
+  query.value = address.label;
+  searchResults.value = [];
+  preventReload.value = false;
+  finalSearchObject.value.ville = address.ville;
+  finalSearchObject.value.department = address.departement;
+  finalSearchObject.value.departmentPostalCode = address.departementPostalCode;
+  finalSearchObject.value.region = address.region;
+  emit('finalSearchObject', finalSearchObject.value);
 };
 </script>
 
@@ -89,11 +108,18 @@ export default {
 .search {
   border: 3px solid $secondary;
   border-radius: 35px;
-  font-size: 20px;
+  font-size: 18px;
+  max-height: 65px;
 }
 
 .search-button {
   color: $accent;
+}
+
+.list {
+  border: 3px solid $secondary;
+  border-radius: 20px;
+  font-size: 18px;
 }
 
 @media only screen and (min-device-width : 320px) and (max-device-width : 768px) {
