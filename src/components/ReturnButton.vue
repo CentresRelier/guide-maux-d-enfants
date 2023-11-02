1b3bd4850055783a294f324fd862a1756e8b8ced
MorelVictor <morel.victor1@gmail.com>
2023-11-02 16:06:09 +0100

    feat(returnBtn): add router link and fix bug

--- src/components/ReturnButton.vue
+++ src/components/ReturnButton.vue
@@ -1,19 +1,36 @@
 <template>
-  <div>
-    <q-btn @click="GoToHomePage" flat>
-      <img class="img" src="/statics/return.svg" />
-    </q-btn>
+  <div class="container">
+    <router-link
+      class="link"
+      :to="{ name: 'home' }">
+      <div>
+        <img class="img" src="/statics/return.svg" />
+      </div>
+    </router-link>
   </div>
 </template>
 
 <script setup>
-function GoToHomePage() {
-  window.location = '/';
-}
 </script>
+
 <style scoped>
 .img {
   height: 40px;
   width: 40px;
+}
+
+.container {
+  width: 40px;
+  height: 40px;
+  margin-left: 10px;
+  border-radius: 200px;
+  transition: background-color 0.3s;
+  text-align-last: center;
+  z-index: 100;
+}
+
+.container:hover {
+  background-color: rgba(38, 37, 108, 0.3);
+  cursor: pointer;
 }
 </style>
